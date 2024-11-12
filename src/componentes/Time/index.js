2195e2678140bbce6573e67a4d34a26a9b101a1d
MatheusGrenat <matheusgrenat@yahoo.com.br>
2024-11-12 19:12:54 -0300

    Criando o input para mudar a cor secundaria do componente time

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -6,6 +6,7 @@ const Time = ({ time, colaboradores, aoDeletar }) => {
 
         colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
             <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
+            <input value={time.corSecundaria} type='color' className='input-cor' />
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) => {
                     return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={aoDeletar} />
