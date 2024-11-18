754f3b96e3341856c2e377b556183c9da627b5f3
MatheusGrenat <matheusgrenat@yahoo.com.br>
2024-11-18 19:36:38 -0300

    Adcionando a funcao de remover um colaborador e concertando o nome dos times

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -5,7 +5,7 @@ import hexToRgba from 'hex-to-rgba';
 const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
     return (
         colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: hexToRgba(time.cor, '0.6') }}>
-            <h3 style={{ borderColor: time.cor }}>{time.id}</h3>
+            <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
             <input onChange={evento => mudarCor(evento.target.value, time.id)} value={time.cor} type='color' className='input-cor' />
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) => {
