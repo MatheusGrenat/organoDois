285452a328f3117e2b61533d4aaee93995d84dd3
MatheusGrenat <matheusgrenat@yahoo.com.br>
2024-11-18 19:25:06 -0300

    Adicionando a API uuidv4 e adicionando no sistema

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,15 +1,15 @@
 import Colaborador from '../Colaborador'
 import './styles.css'
+import hexToRgba from 'hex-to-rgba';
 
 const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
     return (
-
-        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
-            <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
-            <input onChange={evento => mudarCor(evento.target.value, time.nome)} value={time.corSecundaria} type='color' className='input-cor' />
+        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: hexToRgba(time.cor, '0.6') }}>
+            <h3 style={{ borderColor: time.cor }}>{time.id}</h3>
+            <input onChange={evento => mudarCor(evento.target.value, time.id)} value={time.cor} type='color' className='input-cor' />
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) => {
-                    return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={aoDeletar} />
+                    return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.cor} aoDeletar={aoDeletar} />
                 })}
             </div>
         </section>
